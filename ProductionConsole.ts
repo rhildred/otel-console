4998d289239e153c3a4ffe510b63e4112162ae41
Rich Hildred <rhildred@ip-172-31-5-173.us-west-2.compute.internal>
2018-05-15 11:48:34 -0400

    got firebase largely working

--- ProductionConsole.ts
+++ ProductionConsole.ts
@@ -1,18 +1,36 @@
-import {Console} from './index' ;
+import firebase = require("firebase-admin");
+import { Console } from './index';
 
-export class ProductionConsole extends Console{
-    private _bLogging :boolean = false;
-    logging(bLogging : boolean){
+export class ProductionConsole extends Console {
+    private _bLogging: boolean = false;
+    logging(bLogging: boolean) {
         this._bLogging = bLogging;
     }
-    log(sOutput: string){
-        if(this._bLogging){
+    private _sUrl: string = "";
+    private _oRef!: firebase.database.Reference;
+    log(sOutput: string) {
+        if (this._bLogging) {
             super.log(sOutput);
         }
+        if(this._sUrl) {
+            this._oRef.push({message: sOutput, 
+                timestamp: new Date().toISOString()})
+        }
     }
-    private _sUrl: string = "";
-    saveLog(sUrl: string){
+    saveLog(sUrl: string) {
         this._sUrl = sUrl;
-        this.log(this._sUrl + " is where we are logging");
+        let serviceAccount = require("./privatekey.json");
+
+        firebase.initializeApp({
+            credential: firebase.credential.cert(serviceAccount),
+            databaseURL: this._sUrl
+        });
+
+
+        let db = firebase.database();
+
+        this._oRef = db.ref();
+
+        this.log("Firebase initialized");
     }
 }
